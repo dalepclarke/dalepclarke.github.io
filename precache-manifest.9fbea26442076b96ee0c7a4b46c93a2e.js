58fd25eb70a998ef9125a66cfee73eeaae834b70
dalepclarke <daleclarke2962@gmail.com>
2024-08-27 17:38:21 +1000

    Create precache-manifest.9fbea26442076b96ee0c7a4b46c93a2e.js

---
+++ precache-manifest.9fbea26442076b96ee0c7a4b46c93a2e.js
@@ -0,0 +1,722 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699f9cadf4a888617e63",
+    "url": "/css/about.4a6da294.css"
+  },
+  {
+    "revision": "3da42669118adda102c9",
+    "url": "/css/about~accommodation~contact-us~danger-ratings~environmental-policy~explore~frequently-asked-question~ffe81979.fa037b2c.css"
+  },
+  {
+    "revision": "526fe93333f5cb3dd33b",
+    "url": "/css/accommodation-home.6eb7dbb3.css"
+  },
+  {
+    "revision": "6fd6174bf521de9a54eb",
+    "url": "/css/accommodation.1996488d.css"
+  },
+  {
+    "revision": "aa2cad1ae3ca988a2516",
+    "url": "/css/activities.2de36adb.css"
+  },
+  {
+    "revision": "7a98daa2e607cdfe8aac",
+    "url": "/css/app.e1d24f06.css"
+  },
+  {
+    "revision": "1f7998d50771854f7b31",
+    "url": "/css/bird-watching.aa44c84b.css"
+  },
+  {
+    "revision": "41417fb1d5cdc5e628a9",
+    "url": "/css/booking.8269eeb7.css"
+  },
+  {
+    "revision": "ff00729b5c8e8136d9aa",
+    "url": "/css/booking~news-feed~news-post.5fa3acf3.css"
+  },
+  {
+    "revision": "24bfe72c25ccf36a895d",
+    "url": "/css/bunkroom.c082cdfc.css"
+  },
+  {
+    "revision": "bfe238e481c7bc0287fd",
+    "url": "/css/chunk-21255a71.b55a120b.css"
+  },
+  {
+    "revision": "24397d425c993fdb4e10",
+    "url": "/css/chunk-vendors.8d647278.css"
+  },
+  {
+    "revision": "6b5ee16b9688be5204ed",
+    "url": "/css/contact-us.f81c0b4e.css"
+  },
+  {
+    "revision": "db31378ffbb8125f9cd1",
+    "url": "/css/danger-ratings.f015d40b.css"
+  },
+  {
+    "revision": "e5db5af850214f96d365",
+    "url": "/css/deluxe-garden.498cfc75.css"
+  },
+  {
+    "revision": "34732cef4474014af542",
+    "url": "/css/environmental-policy.ae627cf3.css"
+  },
+  {
+    "revision": "7bb8e0fa347d287f779d",
+    "url": "/css/explore.16ce0a01.css"
+  },
+  {
+    "revision": "f23d42db0f86e5e42efa",
+    "url": "/css/flora.5c99d197.css"
+  },
+  {
+    "revision": "82de1491496f3bd8dbb7",
+    "url": "/css/frequently-asked-questions.7da1d6f6.css"
+  },
+  {
+    "revision": "20299e7be20ff44a80b2",
+    "url": "/css/functions.3a22f320.css"
+  },
+  {
+    "revision": "dc5bf8a380d642531b1d",
+    "url": "/css/gallery.8cc7deb7.css"
+  },
+  {
+    "revision": "da191b3c8889a2e09354",
+    "url": "/css/groups.6fef4f35.css"
+  },
+  {
+    "revision": "826791a95c69fc1fc440",
+    "url": "/css/history.7397bff9.css"
+  },
+  {
+    "revision": "cf8f6e8fc6f8df41c5b7",
+    "url": "/css/home.f945de7b.css"
+  },
+  {
+    "revision": "9353df35dfba979357c5",
+    "url": "/css/location.b5431586.css"
+  },
+  {
+    "revision": "06e81efefbed63e3bdc4",
+    "url": "/css/location~trails.f9109212.css"
+  },
+  {
+    "revision": "3e34bf08ad44b7cd9191",
+    "url": "/css/lodge.0dbd8638.css"
+  },
+  {
+    "revision": "73f4b94dd7a645a085fb",
+    "url": "/css/malleefowl.1a14eb78.css"
+  },
+  {
+    "revision": "c7e1995f17e85ec8ee40",
+    "url": "/css/news-feed.3f338189.css"
+  },
+  {
+    "revision": "cc1df8aed064bc919a22",
+    "url": "/css/news-post.00bc46a1.css"
+  },
+  {
+    "revision": "e177563a11fd083ed8b1",
+    "url": "/css/news.6832bf89.css"
+  },
+  {
+    "revision": "68f0768363ecb195eb86",
+    "url": "/css/not-found.85dd106c.css"
+  },
+  {
+    "revision": "d2d8d7b4854f155f5aed",
+    "url": "/css/powered-camping.566de58d.css"
+  },
+  {
+    "revision": "7409fedae35b33453a3e",
+    "url": "/css/privacy-policy.9553aae0.css"
+  },
+  {
+    "revision": "e26d91103e9768ee1fee",
+    "url": "/css/protected.1a268b55.css"
+  },
+  {
+    "revision": "4a89c0c0c622501049b7",
+    "url": "/css/school.07140b83.css"
+  },
+  {
+    "revision": "7b9cdcf365a177d695eb",
+    "url": "/css/standard-ensuite.c3b8d9a1.css"
+  },
+  {
+    "revision": "a95a1482b819f1460dbd",
+    "url": "/css/stargazing.e4f34bbf.css"
+  },
+  {
+    "revision": "b4965d2c6f9349b1ff51",
+    "url": "/css/terms-and-conditions.e0bbbb58.css"
+  },
+  {
+    "revision": "02e8e943e7fc61aed071",
+    "url": "/css/trails.5585880c.css"
+  },
+  {
+    "revision": "9f5f5fb739e3fd6f2bff",
+    "url": "/css/unpowered-camping.2cdcc970.css"
+  },
+  {
+    "revision": "aa851f41ce17a97f4516",
+    "url": "/css/weddings.995b5275.css"
+  },
+  {
+    "revision": "055eabd11f5cf5a9b187",
+    "url": "/css/wildlife.a0eafd41.css"
+  },
+  {
+    "revision": "39bfea5e86f5f41c9d2896dbbed6791b",
+    "url": "/fonts/MaterialIcons-Regular.39bfea5e.woff"
+  },
+  {
+    "revision": "ae8b1248595e70a828b880b9c56963da",
+    "url": "/fonts/MaterialIcons-Regular.ae8b1248.eot"
+  },
+  {
+    "revision": "b073f5972d9c4cc1b8ae8e071e441376",
+    "url": "/fonts/MaterialIcons-Regular.b073f597.woff2"
+  },
+  {
+    "revision": "b9968c1adf542baec8d2b298d3ff437c",
+    "url": "/fonts/MaterialIcons-Regular.b9968c1a.ttf"
+  },
+  {
+    "revision": "062070972fbad0f4fbb3505e6d013a39",
+    "url": "/fonts/primeicons.06207097.woff"
+  },
+  {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "/fonts/primeicons.acb19296.ttf"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "/fonts/primeicons.c08d00ca.eot"
+  },
+  {
+    "revision": "c0929a1e055f2ceb5c889501c24a24e1",
+    "url": "/fonts/primeicons.c0929a1e.woff2"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/fonts/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/fonts/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/fonts/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/fonts/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "af45d8de7eb2675c0cb6d89d9a0352e6",
+    "url": "/img/01bf00da-49c8-48c6-8142-c273a0e89efd.af45d8de.png"
+  },
+  {
+    "revision": "e4a1ca5b0458809c0ee6a46a946e5fa1",
+    "url": "/img/0b8d744c-ccdb-4056-bc9f-60cb793624ae.e4a1ca5b.png"
+  },
+  {
+    "revision": "fbb476a0f2e5aef7e366d3a51140e4a8",
+    "url": "/img/0e524ae6-f6b8-45b5-8d03-4bc526119347.fbb476a0.png"
+  },
+  {
+    "revision": "a3add5f8907a2375341885590b86e93c",
+    "url": "/img/1-malleefowl-on-mound2-transformed.a3add5f8.jpeg"
+  },
+  {
+    "revision": "36e0261ce90b057563016e734d6e9e45",
+    "url": "/img/10342489_10152144577461872_6101071351039110188_n.36e0261c.jpg"
+  },
+  {
+    "revision": "ae2b712ce61201736ffb15a905cd518c",
+    "url": "/img/11879128_10153169942431872_7604745650969256972_o.ae2b712c.jpg"
+  },
+  {
+    "revision": "922a97a6f83327e473e4f4c8cedb4b70",
+    "url": "/img/1bab1fb9-e9c9-439f-919b-b06646518e15.922a97a6.png"
+  },
+  {
+    "revision": "c8d50efeaba83f63d0a4938dcd9a8b9b",
+    "url": "/img/229d756f-f046-4d2d-8afa-6c9066a06a1d.c8d50efe.png"
+  },
+  {
+    "revision": "1ee382dd921f68eda3ddf396a817e72a",
+    "url": "/img/38e7e1f5-1449-4ae2-80c2-8b74b692d5bd.1ee382dd.png"
+  },
+  {
+    "revision": "f4293254369ab43fe4468ade8526ef33",
+    "url": "/img/3e27b084-6fac-42df-b203-6f7c778a9109.f4293254.png"
+  },
+  {
+    "revision": "a2dff31279141b406320f60a25e1fbfe",
+    "url": "/img/5167a178-b962-408f-a3af-70bcf516e437.a2dff312.png"
+  },
+  {
+    "revision": "40ecf318fcd71b97d9f73617bb01e49e",
+    "url": "/img/55156b83-4fc0-4400-aa62-463f7dc3a326.40ecf318.png"
+  },
+  {
+    "revision": "b44d6878e7f99220fffcbb2c2a9003b8",
+    "url": "/img/83cb8ea0-d144-4306-ba61-ccd0da91e9a1.b44d6878.png"
+  },
+  {
+    "revision": "daf81e68e7672d99abf79db2dee13c4c",
+    "url": "/img/958e5f48-abd0-4f07-ba5a-7e948d62472a.daf81e68.png"
+  },
+  {
+    "revision": "261b4596e22afe7d939dd0189af538ba",
+    "url": "/img/9b7b3ff4-fd06-44c7-a55d-46e7c871821c.261b4596.png"
+  },
+  {
+    "revision": "bd78499a6b870244d2c26229797c2063",
+    "url": "/img/Lodge-Construction.bd78499a.jpg"
+  },
+  {
+    "revision": "478312b494d2863fb98e65b7a21a3c1b",
+    "url": "/img/Malleefowl1.478312b4.jpg"
+  },
+  {
+    "revision": "40182da8805196648f41b76401264f74",
+    "url": "/img/WhiimpyEMU2.40182da8.jpg"
+  },
+  {
+    "revision": "e2025502aa56fd8277674b465c3c060f",
+    "url": "/img/activities2.e2025502.jpg"
+  },
+  {
+    "revision": "ac9bf6fdbcc4e88e103af1cf6d5ff26e",
+    "url": "/img/bathroom1.ac9bf6fd.jpeg"
+  },
+  {
+    "revision": "dc0068f045264d5625039127e569b79f",
+    "url": "/img/bathroom1.dc0068f0.jpeg"
+  },
+  {
+    "revision": "290667e22df3f69b2937cfeaedcd14e0",
+    "url": "/img/bathroom2.290667e2.jpeg"
+  },
+  {
+    "revision": "e1eb5a54e5f58fff8e24484d6ca8794f",
+    "url": "/img/bird_watching.e1eb5a54.png"
+  },
+  {
+    "revision": "8c2cb6048536e8c3b5470cb2c5450d4c",
+    "url": "/img/bunks1.8c2cb604.jpg"
+  },
+  {
+    "revision": "f3ed208cc7747e717abb92a438f9de51",
+    "url": "/img/c2545410-1a24-46f8-a8e6-dd57939946c9.f3ed208c.png"
+  },
+  {
+    "revision": "70d96c8fd49fdd6b5bab6adda7cc9862",
+    "url": "/img/cfa9aa4e-6777-4ba9-8eff-6f30909c76d9.70d96c8f.png"
+  },
+  {
+    "revision": "1653798309a16ccd106accb447191032",
+    "url": "/img/cover.16537983.jpg"
+  },
+  {
+    "revision": "2dd95ccb816abd9d28171473f6ec7fdc",
+    "url": "/img/e0305da0-da6a-4c03-8ec5-d85225ec2958.2dd95ccb.png"
+  },
+  {
+    "revision": "422a0fdaa1bc9c1f98926d1bf16606ba",
+    "url": "/img/eae18685-fa5e-43a3-a60a-4ea72c5b9ae2.422a0fda.png"
+  },
+  {
+    "revision": "2a617b56494a12decb3872539b322647",
+    "url": "/img/exterior1.2a617b56.webp"
+  },
+  {
+    "revision": "49bfd907f714cdc301a607a077323498",
+    "url": "/img/exterior2.49bfd907.jpeg"
+  },
+  {
+    "revision": "0651e3841190575bfe03aa244f46aaa8",
+    "url": "/img/f2e1e952-14ec-464e-9aec-73fcda5606c4.0651e384.png"
+  },
+  {
+    "revision": "9d3ba6318c79a362752708ec638df25e",
+    "url": "/img/family1.9d3ba631.webp"
+  },
+  {
+    "revision": "4dee24428700a897d52bf22352511d10",
+    "url": "/img/family2.4dee2442.webp"
+  },
+  {
+    "revision": "bee48fae09ceaafea78c8e3531488b65",
+    "url": "/img/ff029aa8-608e-49a5-a53f-3445d4008329.bee48fae.png"
+  },
+  {
+    "revision": "1546b202cfb20e1daaf3108a73ec4d53",
+    "url": "/img/flora2.1546b202.jpg"
+  },
+  {
+    "revision": "60a28b1eddac5a46196a778967f082cb",
+    "url": "/img/functions2.60a28b1e.jpg"
+  },
+  {
+    "revision": "d36a2e710a02ca376f6b82e8d502c383",
+    "url": "/img/groups.d36a2e71.jpg"
+  },
+  {
+    "revision": "45260bca896aca34115902b65a2f3d84",
+    "url": "/img/image.45260bca.png"
+  },
+  {
+    "revision": "dcbb3057414355cf0db29e58df3613d5",
+    "url": "/img/king1.dcbb3057.jpeg"
+  },
+  {
+    "revision": "1cc807177e552c967cb0a2c343b14a02",
+    "url": "/img/king2.1cc80717.jpg"
+  },
+  {
+    "revision": "1e0c2c3525ea99a762b54a8a638ec134",
+    "url": "/img/logo.1e0c2c35.png"
+  },
+  {
+    "revision": "db5228cc9a4626ba1812f72a23ed09d6",
+    "url": "/img/malleefowl.db5228cc.jpg"
+  },
+  {
+    "revision": "1ee1bff063d0d4811ee895ec0e267f99",
+    "url": "/img/map.1ee1bff0.png"
+  },
+  {
+    "revision": "006a30097502ee028b7086e4dd1828fb",
+    "url": "/img/powered1.006a3009.jpg"
+  },
+  {
+    "revision": "1d854e3ab2c37173804a0d1d1af229ee",
+    "url": "/img/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "c37a7f57ab28da20014fe88c646ed6f5",
+    "url": "/img/schools2.c37a7f57.jpg"
+  },
+  {
+    "revision": "b26f1f06e2685b362665d1f2d02240da",
+    "url": "/img/stargazing2.b26f1f06.jpg"
+  },
+  {
+    "revision": "bb56551924306e964869cf3b195fdb2d",
+    "url": "/img/triple1.bb565519.webp"
+  },
+  {
+    "revision": "0b0e048a0de1a1f007984592a398d461",
+    "url": "/img/twin1.0b0e048a.jpeg"
+  },
+  {
+    "revision": "36ccc9c555c16bde94c43c08c4349cb7",
+    "url": "/img/twin1.36ccc9c5.jpeg"
+  },
+  {
+    "revision": "ef6ed91b7541d657a7fe5757e28d678f",
+    "url": "/img/twin2.ef6ed91b.jpeg"
+  },
+  {
+    "revision": "378b2cc83b55bac907ecd0b53fa8835d",
+    "url": "/img/unpowered1.378b2cc8.jpeg"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/img/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "5580d918142d5259dbe6ddb9d06fb61a",
+    "url": "/img/weddings.5580d918.jpg"
+  },
+  {
+    "revision": "06cbd02dc9ec14468e8c2d4a8b2cb43c",
+    "url": "/img/wildlife.06cbd02d.jpg"
+  },
+  {
+    "revision": "4fa192c7b9a47b2966e5e81958a1eb35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "699f9cadf4a888617e63",
+    "url": "/js/about.35272ecb.js"
+  },
+  {
+    "revision": "3da42669118adda102c9",
+    "url": "/js/about~accommodation~contact-us~danger-ratings~environmental-policy~explore~frequently-asked-question~ffe81979.9cdda4a7.js"
+  },
+  {
+    "revision": "526fe93333f5cb3dd33b",
+    "url": "/js/accommodation-home.9f7aaf2c.js"
+  },
+  {
+    "revision": "6fd6174bf521de9a54eb",
+    "url": "/js/accommodation.a6f42332.js"
+  },
+  {
+    "revision": "aa2cad1ae3ca988a2516",
+    "url": "/js/activities.661ce39a.js"
+  },
+  {
+    "revision": "7a98daa2e607cdfe8aac",
+    "url": "/js/app.09e2da81.js"
+  },
+  {
+    "revision": "1f7998d50771854f7b31",
+    "url": "/js/bird-watching.2db78c00.js"
+  },
+  {
+    "revision": "41417fb1d5cdc5e628a9",
+    "url": "/js/booking.beae2d54.js"
+  },
+  {
+    "revision": "fe92e67ab2622ea27471",
+    "url": "/js/booking~contact-us.8c122032.js"
+  },
+  {
+    "revision": "ff00729b5c8e8136d9aa",
+    "url": "/js/booking~news-feed~news-post.56733ae7.js"
+  },
+  {
+    "revision": "24bfe72c25ccf36a895d",
+    "url": "/js/bunkroom.2d7c0d03.js"
+  },
+  {
+    "revision": "bfe238e481c7bc0287fd",
+    "url": "/js/chunk-21255a71.73a8614e.js"
+  },
+  {
+    "revision": "71178a2e10c373b2df2a",
+    "url": "/js/chunk-2d0c4832.54ff36c8.js"
+  },
+  {
+    "revision": "34e5902c67980186ee13",
+    "url": "/js/chunk-44869a72.b8ee09aa.js"
+  },
+  {
+    "revision": "3c4f946f838c277d255b",
+    "url": "/js/chunk-473846b0.096e2d85.js"
+  },
+  {
+    "revision": "bde7e624646d00455389",
+    "url": "/js/chunk-4f14e4ea.eb45cb5a.js"
+  },
+  {
+    "revision": "3dc1c67c2951ecbb8660",
+    "url": "/js/chunk-5e8062ac.1a22d143.js"
+  },
+  {
+    "revision": "8b7d9323379f2799ffcc",
+    "url": "/js/chunk-7121aaca.f15a4abf.js"
+  },
+  {
+    "revision": "d7f669c367d97f22c6f8",
+    "url": "/js/chunk-e9d966a6.0f1cfcd7.js"
+  },
+  {
+    "revision": "24397d425c993fdb4e10",
+    "url": "/js/chunk-vendors.a5379ff9.js"
+  },
+  {
+    "revision": "6b5ee16b9688be5204ed",
+    "url": "/js/contact-us.5632a6f4.js"
+  },
+  {
+    "revision": "db31378ffbb8125f9cd1",
+    "url": "/js/danger-ratings.288ef1c4.js"
+  },
+  {
+    "revision": "e5db5af850214f96d365",
+    "url": "/js/deluxe-garden.1ca831ed.js"
+  },
+  {
+    "revision": "34732cef4474014af542",
+    "url": "/js/environmental-policy.195f7643.js"
+  },
+  {
+    "revision": "7bb8e0fa347d287f779d",
+    "url": "/js/explore.7f028a9f.js"
+  },
+  {
+    "revision": "f23d42db0f86e5e42efa",
+    "url": "/js/flora.89c43a6f.js"
+  },
+  {
+    "revision": "82de1491496f3bd8dbb7",
+    "url": "/js/frequently-asked-questions.2f62a3d9.js"
+  },
+  {
+    "revision": "20299e7be20ff44a80b2",
+    "url": "/js/functions.71595400.js"
+  },
+  {
+    "revision": "dc5bf8a380d642531b1d",
+    "url": "/js/gallery.b9f988ca.js"
+  },
+  {
+    "revision": "da191b3c8889a2e09354",
+    "url": "/js/groups.0cb8bb2d.js"
+  },
+  {
+    "revision": "826791a95c69fc1fc440",
+    "url": "/js/history.5a65a3b5.js"
+  },
+  {
+    "revision": "cf8f6e8fc6f8df41c5b7",
+    "url": "/js/home.a7614356.js"
+  },
+  {
+    "revision": "9353df35dfba979357c5",
+    "url": "/js/location.2657fcdf.js"
+  },
+  {
+    "revision": "06e81efefbed63e3bdc4",
+    "url": "/js/location~trails.420d5b99.js"
+  },
+  {
+    "revision": "3e34bf08ad44b7cd9191",
+    "url": "/js/lodge.80cc6aaa.js"
+  },
+  {
+    "revision": "73f4b94dd7a645a085fb",
+    "url": "/js/malleefowl.ffa8e1cf.js"
+  },
+  {
+    "revision": "c7e1995f17e85ec8ee40",
+    "url": "/js/news-feed.19fed241.js"
+  },
+  {
+    "revision": "cc1df8aed064bc919a22",
+    "url": "/js/news-post.3681fa29.js"
+  },
+  {
+    "revision": "e177563a11fd083ed8b1",
+    "url": "/js/news.cb17b667.js"
+  },
+  {
+    "revision": "68f0768363ecb195eb86",
+    "url": "/js/not-found.226232a7.js"
+  },
+  {
+    "revision": "d2d8d7b4854f155f5aed",
+    "url": "/js/powered-camping.0db1823f.js"
+  },
+  {
+    "revision": "7409fedae35b33453a3e",
+    "url": "/js/privacy-policy.bf9243fe.js"
+  },
+  {
+    "revision": "e26d91103e9768ee1fee",
+    "url": "/js/protected.5bc9ec92.js"
+  },
+  {
+    "revision": "4a89c0c0c622501049b7",
+    "url": "/js/school.3fa68d84.js"
+  },
+  {
+    "revision": "7b9cdcf365a177d695eb",
+    "url": "/js/standard-ensuite.b5dccdfa.js"
+  },
+  {
+    "revision": "a95a1482b819f1460dbd",
+    "url": "/js/stargazing.7fa9c212.js"
+  },
+  {
+    "revision": "b4965d2c6f9349b1ff51",
+    "url": "/js/terms-and-conditions.6c488f65.js"
+  },
+  {
+    "revision": "02e8e943e7fc61aed071",
+    "url": "/js/trails.30202d1c.js"
+  },
+  {
+    "revision": "9f5f5fb739e3fd6f2bff",
+    "url": "/js/unpowered-camping.0e679d44.js"
+  },
+  {
+    "revision": "aa851f41ce17a97f4516",
+    "url": "/js/weddings.e8589865.js"
+  },
+  {
+    "revision": "055eabd11f5cf5a9b187",
+    "url": "/js/wildlife.daa5820c.js"
+  },
+  {
+    "revision": "fc22ee23cbeed12bfbc0548dc4f1407f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4978fee544a7916a7da9169b53ca596f",
+    "url": "/manifest.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3d9446f210892af971a3854d0f984ae9",
+    "url": "/themes/mdc-dark-indigo/fonts/roboto-v20-latin-ext_latin-500.woff"
+  },
+  {
+    "revision": "15431381890720a5a4b62b33c8ae06c5",
+    "url": "/themes/mdc-dark-indigo/fonts/roboto-v20-latin-ext_latin-500.woff2"
+  },
+  {
+    "revision": "d0cc855e64aca1072711f49d1a38ca0c",
+    "url": "/themes/mdc-dark-indigo/fonts/roboto-v20-latin-ext_latin-700.woff"
+  },
+  {
+    "revision": "52bb58c8cb04cf3eea4f9ac0afa1d1f6",
+    "url": "/themes/mdc-dark-indigo/fonts/roboto-v20-latin-ext_latin-700.woff2"
+  },
+  {
+    "revision": "d679a90608a65cc9394f67448ca2094a",
+    "url": "/themes/mdc-dark-indigo/fonts/roboto-v20-latin-ext_latin-regular.woff"
+  },
+  {
+    "revision": "c5bf51b68dc9fd7fe944d8947fe12518",
+    "url": "/themes/mdc-dark-indigo/fonts/roboto-v20-latin-ext_latin-regular.woff2"
+  },
+  {
+    "revision": "0c0dd3d77ef1eb446fb82dd865c54c11",
+    "url": "/themes/mdc-dark-indigo/theme.css"
+  },
+  {
+    "revision": "3d9446f210892af971a3854d0f984ae9",
+    "url": "/themes/mdc-light-indigo/fonts/roboto-v20-latin-ext_latin-500.woff"
+  },
+  {
+    "revision": "15431381890720a5a4b62b33c8ae06c5",
+    "url": "/themes/mdc-light-indigo/fonts/roboto-v20-latin-ext_latin-500.woff2"
+  },
+  {
+    "revision": "d0cc855e64aca1072711f49d1a38ca0c",
+    "url": "/themes/mdc-light-indigo/fonts/roboto-v20-latin-ext_latin-700.woff"
+  },
+  {
+    "revision": "52bb58c8cb04cf3eea4f9ac0afa1d1f6",
+    "url": "/themes/mdc-light-indigo/fonts/roboto-v20-latin-ext_latin-700.woff2"
+  },
+  {
+    "revision": "d679a90608a65cc9394f67448ca2094a",
+    "url": "/themes/mdc-light-indigo/fonts/roboto-v20-latin-ext_latin-regular.woff"
+  },
+  {
+    "revision": "c5bf51b68dc9fd7fe944d8947fe12518",
+    "url": "/themes/mdc-light-indigo/fonts/roboto-v20-latin-ext_latin-regular.woff2"
+  },
+  {
+    "revision": "b762add74d19619254a1cb94facd7125",
+    "url": "/themes/mdc-light-indigo/theme.css"
+  }
+]);
